b77ac3c5301b6644e49c7e77c510e403ae91fb40
Arnaud Lugand <alugand@gmail.com>
2025-10-13 09:23:17 +0200

    add number

---
+++ number.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstdlib>   
+#include <ctime>     
+#include <iomanip>
+#include <cstdio>
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+    srand(time(0)); // Initialisation du générateur aléatoire
+    ofstream answ("answer_associativity.txt", ios::app); // Ouvre en mode ajout
+    if(!answ.is_open()){
+        cout << "erreur ouverture fichier resultat" << endl;
+    }
+    else {
+        int valeurok=0;
+        int nbexperience=100;
+        for(int i=0;i<nbexperience; i++){
+            float val1 = static_cast<float>(rand());
+            float val2 = static_cast<float>(rand());
+            float val3 = static_cast<float>(rand());
+            if(val1+(val2+val3)==(val1+val2)+val3){
+                valeurok++;
+            }
+        }
+        float resultat = static_cast<float>(valeurok)/nbexperience;
+        answ << resultat <<endl;
+        answ.close();
+    }
+    return 0;
+}
