717e2b859278e9a5bf18b373955916f2a9e5d1fa
Maelenn Le Sommer <maelenn.le-sommer@insa-rennes.fr>
2025-10-20 09:16:31 +0200

    add type

--- number.cpp
+++ number.cpp
@@ -17,25 +17,37 @@ int main() {
         int valeurok=0;
         int nbexperience=10000;
         for(int i=0;i<nbexperience; i++){
-            double val1 = static_cast<double>(rand());
-            double val2 = static_cast<double>(rand());
-            double val3 = static_cast<double>(rand());
+            float val1 = static_cast<float>(rand())/ static_cast<float>(RAND_MAX);
+            float val2 = static_cast<float>(rand())/ static_cast<float>(RAND_MAX);
+            float val3 = static_cast<float>(rand())/ static_cast<float>(RAND_MAX);
             if(val1+(val2+val3)==(val1+val2)+val3){
                 valeurok++;
             }
         }
-        double resultat = static_cast<double>(valeurok)/nbexperience;
-        answ << "Avec des double : "<<resultat <<endl;
+        float resultat = static_cast<float>(valeurok)/nbexperience;
+        answ << "Avec des float : "<<resultat <<endl;
+        valeurok=0;
         for(int i=0;i<nbexperience; i++){
-            long double val1 = static_cast<long double>(rand());
-            long double val2 = static_cast<long double>(rand());
-            long double val3 = static_cast<long double>(rand());
+            double val1 = static_cast<double>(rand())/ RAND_MAX;
+            double val2 = static_cast<double>(rand())/ RAND_MAX;
+            double val3 = static_cast<double>(rand())/ RAND_MAX;
             if(val1+(val2+val3)==(val1+val2)+val3){
                 valeurok++;
             }
         }
-        resultat = static_cast<long double>(valeurok)/nbexperience;
-        answ << "Avec des long double : "<<resultat <<endl;
+        double resultat2 = static_cast<double>(valeurok)/nbexperience;
+        answ << "Avec des double : "<<resultat2 <<endl;
+        valeurok=0;
+        for(int i=0;i<nbexperience; i++){
+            long double val1 = static_cast<long double>(rand())/ RAND_MAX;
+            long double val2 = static_cast<long double>(rand())/ RAND_MAX;
+            long double val3 = static_cast<long double>(rand())/ RAND_MAX;
+            if(val1+(val2+val3)==(val1+val2)+val3){
+                valeurok++;
+            }
+        }
+        long double resultat3 = static_cast<long double>(valeurok)/nbexperience;
+        answ << "Avec des long double : "<<resultat3 <<endl;
         answ.close();
     }
     return 0;
