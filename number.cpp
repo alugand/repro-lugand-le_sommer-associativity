228cdb970afb3294aa3031753fbbde440e449860
Maelenn Le Sommer <maelenn.le-sommer@insa-rennes.fr>
2025-10-20 09:41:14 +0200

    fix nb ex

--- number.cpp
+++ number.cpp
@@ -15,11 +15,11 @@ int main() {
     }
     else {
         int valeurok=0;
-        int nbexperience=1000;
+        int nbexperience=100000;
         for(int i=0;i<nbexperience; i++){
-            float val1 = static_cast<float>(rand());
-            float val2 = static_cast<float>(rand());
-            float val3 = static_cast<float>(rand());
+            float val1 = static_cast<float>(rand())/static_cast<float>(RAND_MAX);
+            float val2 = static_cast<float>(rand())/static_cast<float>(RAND_MAX);
+            float val3 = static_cast<float>(rand())/static_cast<float>(RAND_MAX);
             if(val1+(val2+val3)==(val1+val2)+val3){
                 valeurok++;
             }
